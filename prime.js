db4fbe03c78c3d31bf68fee2c9db70831ef8a559
unknown <akshay23lonare@gmail.com>
2022-09-20 18:05:42 +0530

    code fix

--- prime.js
+++ prime.js
@@ -1,7 +1,22 @@
 function prime(num){
+let counter=0;
     for (let i=1;i<=num;i++){
         if(num%i==0){
+          counter++; 
             
         }
     }
+    if(counter==2){
+       return true
+    }
+      return false
+
+}
+
+let ans=prime(7);
+if (ans==true){
+ console.log("Prime");
+}
+else{
+   console.log("Not Prime")
 }
