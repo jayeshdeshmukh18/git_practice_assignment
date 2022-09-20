ac792c11ef9b8dbde43b547f5e588ad904519c0a
jayeshdeshmukh18 <deshmukhjayesh507@gmail.com>
2022-09-20 21:16:11 +0530

    code fix by jayesh

--- prime.js
+++ prime.js
@@ -1,7 +1,19 @@
 function prime(num){
+    let factors=0;
     for (let i=1;i<=num;i++){
         if(num%i==0){
-            
+            factors++;
         }
     }
+    if(factors==2){
+        return true;
+    }
+    return false;
+}
+
+let answer=prime(7);
+if(answer==true){
+    console.log("prime")
+}else{
+    console.log("Not prime")
 }
